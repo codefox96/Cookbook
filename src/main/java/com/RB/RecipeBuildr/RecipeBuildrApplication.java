69185f515915183c3eb2965c2f8977ba250ccb5c
codefox96 <jordangilpin1@gmail.com>
2022-04-06 10:48:28 -0400

    initpush

---
+++ src/main/java/com/RB/RecipeBuildr/RecipeBuildrApplication.java
@@ -0,0 +1,13 @@
+package com.RB.RecipeBuildr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeBuildrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeBuildrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RB/RecipeBuildr/RecipeBuildrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RB.RecipeBuildr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBuildrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
