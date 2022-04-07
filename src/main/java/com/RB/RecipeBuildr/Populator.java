69498d5acb3b9450f0e12e610f218217ef637a4a
codefox96 <jordangilpin1@gmail.com>
2022-04-07 11:02:55 -0400

    Started populator

--- src/main/java/com/RB/RecipeBuildr/Populator.java
+++ src/main/java/com/RB/RecipeBuildr/Populator.java
@@ -1,10 +1,23 @@
 package com.RB.RecipeBuildr;
 
+import com.RB.RecipeBuildr.storage.HashtagStorage;
+import com.RB.RecipeBuildr.storage.IngredStorage;
+import com.RB.RecipeBuildr.storage.RecipeStorage;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Populator implements CommandLineRunner {
+    IngredStorage ingredStorage;
+    RecipeStorage recipeStorage;
+    HashtagStorage hashStorage;
+
+    public Populator(IngredStorage ingredStorage, RecipeStorage recipeStorage, HashtagStorage hashStorage) {
+        this.ingredStorage = ingredStorage;
+        this.recipeStorage = recipeStorage;
+        this.hashStorage = hashStorage;
+    }
+
     @Override
     public void run(String... args) throws Exception {
 
